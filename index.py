3c9d1d3e2e149c91f30064b30cdf0cdbf8750869
shewchyk <shewchykj@gmail.com>
2023-11-04 07:39:22 +0100

    while loop update

--- index.py
+++ index.py
@@ -4,35 +4,42 @@
 from playsound import playsound
 import datetime
 from datetime import datetime, date
+import time
 
 warnings = ['/home/andrew/code/time_warning/10mins.mp3','/home/andrew/code/time_warning/5mins.mp3','/home/andrew/code/time_warning/3mins.mp3','/home/andrew/code/time_warning/90secs.mp3','/home/andrew/code/time_warning/60secs.mp3','/home/andrew/code/time_warning/30secs.mp3','/home/andrew/code/time_warning/10secs.mp3','/home/andrew/code/time_warning/goinglive.mp3']
 
-meeting_time = datetime(2023, 10, 24, 8, 30, 00)
-timenow = datetime.now()
-trigger = meeting_time - timenow
+while True:
+ meeting_time = datetime(2023, 11, 4, 7, 35, 00)
+ timenow = datetime.now()
+ trigger = meeting_time - timenow
 
-sec_trigger = round(trigger.total_seconds())
+ sec_trigger = round(trigger.total_seconds())
 
-print(meeting_time)
-print(sec_trigger)
 
-def test(seconds):
-    if seconds == 3:
+ print(meeting_time)
+ print(sec_trigger)
+
+ def test(seconds):
+    if seconds < 3 and seconds > 0:
         playsound(warnings[7])
-    elif seconds == 10:
+    elif seconds < 12 and seconds > 8:
         playsound(warnings[6])
-    elif seconds == 30:
+    elif seconds < 32 and seconds > 28:
         playsound(warnings[5])
-    elif seconds == 60:
+    elif seconds < 62 and seconds > 58:
         playsound(warnings[4])
-    elif seconds == 90:
+    elif seconds < 92 and seconds > 88:
         playsound(warnings[3])
-    elif seconds == 180:
+    elif seconds < 182 and seconds > 178:
         playsound(warnings[2])
-    elif seconds == 300:
+    elif seconds < 302 and seconds > 298:
         playsound(warnings[1])
-    elif seconds == 600:
+    elif seconds < 602 and seconds > 598:
         playsound(warnings[0])
     else: pass
 
-test(sec_trigger)
+
+ test(sec_trigger)  
+ time.sleep(2)
+ if sec_trigger < 0:
+     break        
